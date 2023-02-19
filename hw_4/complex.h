89ed222962f8097170e4d0fa8332ba237176357e
dan87131 <122404340+dan87131@users.noreply.github.com>
2023-02-19 00:31:39 -0800

    Update complex.h

--- hw_4/complex.h
+++ hw_4/complex.h
@@ -5,7 +5,7 @@ public:
     double imaginary();
     Complex conjugate();
 
-    // Operator overloading
+    
     Complex operator+(const Complex& other);
     Complex operator*(const Complex& other);
     bool operator==(const Complex& other);
